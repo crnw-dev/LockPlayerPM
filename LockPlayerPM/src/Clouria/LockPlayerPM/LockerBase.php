978b2e8727585f1a9f180ded591785e3b7ac790d
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-02-09 02:45:02 +0800

    Implemented LockerBase.php

--- LockPlayerPM/src/Clouria/LockPlayerPM/LockerBase.php
+++ LockPlayerPM/src/Clouria/LockPlayerPM/LockerBase.php
@@ -5,7 +5,26 @@ declare(strict_types=1);
 
 namespace Clouria\LockPlayerPM;
 
+use function array_merge;
+
 abstract class LockerBase
 {
+
+    /**
+     * @var LockerBase[]
+     */
+    protected array $lockers = [];
+
+    public function addLocker(LockerBase $locker) : void {
+        $this->lockers[] = $locker;
+    }
+
+    public function getEvents() : array {
+        $events = [];
+        foreach ($this->lockers as $locker) {
+            $events = array_merge($events, $locker->getEvents());
+        }
+        return $events;
+    }
 
 }
