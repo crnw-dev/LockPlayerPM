36955aaf0595f38abab382205b4d96adfcea4969
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-02-09 18:44:13 +0800

    Added EventListener.php

---
+++ LockPlayerPM/src/Clouria/LockPlayerPM/EventListener.php
@@ -0,0 +1,18 @@
+<?php
+
+
+declare(strict_types=1);
+
+namespace Clouria\LockPlayerPM;
+
+use pocketmine\event\Listener;
+use pocketmine\player\Player;
+
+class EventListener implements Listener
+{
+
+    public function lock(Player $player) : callable {
+
+    }
+
+}
